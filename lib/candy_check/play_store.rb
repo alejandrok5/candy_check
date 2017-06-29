c94887962cf695bb8869297a220145a34fe0aa37
Alejandro Gonzalez <alejandrok5@gmail.com>
2017-06-29 15:40:57 -0400

    removing outdated play_store

--- lib/candy_check/play_store.rb
+++ lib/candy_check/play_store.rb
@@ -1,15 +1,3 @@
-require 'google/api_client'
-
-require 'candy_check/play_store/discovery_repository'
-require 'candy_check/play_store/client'
-require 'candy_check/play_store/config'
-require 'candy_check/play_store/receipt'
-require 'candy_check/play_store/subscription'
-require 'candy_check/play_store/verification'
-require 'candy_check/play_store/subscription_verification'
-require 'candy_check/play_store/verification_failure'
-require 'candy_check/play_store/verifier'
-
 module CandyCheck
   # Module to request and verify a AppStore receipt
   module PlayStore
